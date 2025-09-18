b83457ab59541f5b778d2cdd4f5177832f630ac3
Jan-Erik Labbas <jamps3@gmail.com>
2025-09-18 13:23:03 +0300

    osa1: uudelleenrenderointi: destrukturointi osa2

--- osa1/uudelleenrenderointi/src/App.jsx
+++ osa1/uudelleenrenderointi/src/App.jsx
@@ -29,12 +29,8 @@ const App = () => {
   )
 }
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 export default App
