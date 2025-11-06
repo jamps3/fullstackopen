fd4c5d534edc27eb3d4a1647fbff4d9ce5e0852c
Jan-Erik Labbas <jamps3@gmail.com>
2025-11-06 11:04:27 +0200

    osa2: 2.11: puhelinluettelo step6

--- osa2/2.6-11.puhelinluettelo/src/App.jsx
+++ osa2/2.6-11.puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
@@ -13,6 +14,14 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
